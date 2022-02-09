f6a98846285e2b7ed7db65aa92926a39428624db
Luc Kury <luc.kury@unibas.ch>
2022-02-09 12:13:20 +0100

    Adds debug statement

--- runtime/src/reinforcement-learning/kmp_rl_agent_factory.cpp
+++ runtime/src/reinforcement-learning/kmp_rl_agent_factory.cpp
@@ -9,8 +9,8 @@ int RLAgentFactory::rlAgentSearch(const std::string& loop_id, int agent_type, do
     //TODO: Make print statement here (with thread ID)
     std::cout << "We got here with " << loop_id << std::endl;
     if (!RLAgentFactory::GetTimesteps().count(loop_id)) {
-        std::cout << "We got here again" << std::endl;
         RLAgentFactory::GetTimesteps().insert(std::make_pair(loop_id, 0));
+        std::cout << "We got here again" << std::endl;
 
         auto agent = create_agent(loop_id, agent_type, portfolio_size, portfolio_size, 6);
         RLAgentFactory::GetAgents().insert(std::make_pair(loop_id, agent));

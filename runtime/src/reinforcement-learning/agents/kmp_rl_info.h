96c42a203e08e8050293ede1ab5529b588dfb9af
Luc Kury <luc.kury@unibas.ch>
2022-02-09 13:09:43 +0100

    Adds initializer list to RLInfo.

--- runtime/src/reinforcement-learning/agents/kmp_rl_info.h
+++ runtime/src/reinforcement-learning/agents/kmp_rl_info.h
@@ -1,12 +1,31 @@
 #pragma once
 
+#include <iostream>
+
 /* Struct to manage data of the reinforcement learning agent */
 class RLInfo {
 public:
+    int state;
+    int trialstate;
+    int *count;
+    double lowTime, highTime;
+    double **qvalue;
+    double bestqvalue;
 
-    RLInfo(int states, int actions) {
+    double alpha;
+    double gamma;
+
+    RLInfo(int states, int actions) : state(0),
+                                      trialstate(0),
+                                      lowTime(-99.0),
+                                      highTime(-999.0),
+                                      bestqvalue(0.0),
+                                      alpha(0.0),
+                                      gamma(0.0)
+    {
+        std::cout << "We got here again 5" << std::endl;
         count = new int[states];
-        qvalue = new double*[states];
+        qvalue = new double *[states];
 
         for (int i = 0; i < states; i++) {
             qvalue[i] = new double[actions];
@@ -16,23 +35,12 @@ public:
             for (int a = 0; a < actions; a++) {
                 count[a] = 0;
                 qvalue[s][a] = -299.0;
-        }
+            }
 
         alpha = std::stod(std::getenv("KMP_RL_ALPHA"));
         gamma = std::stod(std::getenv("KMP_RL_GAMMA"));
     }
 
-    int state{0};
-    int action{0};
-    int trialstate{0};
-    int* count;
-
-    double lowTime{-99.0}, highTime{-999.0};
-    double** qvalue;
-    double bestqvalue{0.0};
-
-    double alpha{};
-    double gamma{};
-
 private:
+
 };

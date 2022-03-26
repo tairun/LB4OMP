3f288b0cc39d6c10b516ab1e5374d01820eea05a
Luc Kury <luc.kury@unibas.ch>
2022-03-26 12:21:13 +0100

    Reverted to CXX14

--- runtime/src/reinforcement-learning/agents/kmp_deepq_learner.cpp
+++ runtime/src/reinforcement-learning/agents/kmp_deepq_learner.cpp
@@ -1,17 +1,16 @@
 #include "kmp_rl_agent.h"
 #include "kmp_deepq_learner.h"
-#include "../../thirdparty/MiniDNN/include/MiniDNN.h"
+//#include "../../thirdparty/MiniDNN/include/MiniDNN.h"
 
 
 // public
-DeepQLearner::DeepQLearner(int num_states, int num_actions) :
-              RLAgent(num_states, num_actions, "DQN Learner", nullptr)
-{
-    // TODO: Implementation
+DeepQLearner::DeepQLearner(int num_states, int num_actions)
+    : RLAgent(num_states, num_actions, "DQN Learner", nullptr) {
+  // TODO: Implementation
 }
 
 // private
-void DeepQLearner::update(int next_state, int next_action, double reward_value)
-{
-    //TODO@kurluc00: Implement
+void DeepQLearner::update(int next_state, int next_action,
+                          double reward_value) {
+  // TODO@kurluc00: Implement
 }
--- runtime/src/reinforcement-learning/initializers/kmp_base_init.h
+++ runtime/src/reinforcement-learning/initializers/kmp_base_init.h
@@ -1,7 +1,8 @@
-class BaseInit
-{
+#pragma once
+
+class BaseInit {
 public:
-    static void init(int* data, int size);
-    static void init(double* data, int size);
-    static void init(double** data, int num_actions, int num_states);
+  static void init(int *data, int size);
+  static void init(double *data, int size);
+  static void init(double **data, int num_actions, int num_states);
 };

7d17f1dd5a5dd2cda7ec0b8d26a8f5ea39f783bf
Luc Kury <luc.kury@unibas.ch>
2022-06-15 15:46:37 +0200

    More updates

--- runtime/src/reinforcement-learning/agents/kmp_q_learner.cpp
+++ runtime/src/reinforcement-learning/agents/kmp_q_learner.cpp
@@ -16,15 +16,18 @@ QLearner::QLearner(int num_states, int num_actions) :
           Agent(num_states, num_actions, "Q-Learner")
 {
     // Initialize first dimension of table
-    std::cout << "[ALearner::QLearner] Using init ..." << std::endl;
+    std::cout << "[QLearner::QLearner] Using init ..." << std::endl;
     pInit->init(q_table, state_space, action_space);
-    std::cout << "[ALearner::QLearner] Done using init." << std::endl;
+    std::cout << "[QLearner::QLearner] Done using init." << std::endl;
     set_table(q_table);
 }
 
 // private
 void QLearner::update(int next_state, int next_action, int actions, double reward_value)
 {
+    std::cout << "[QLearner::update] Using argmax ..." << std::endl;
+    std::cout << "[QLearner::update] Actions ..." << actions << std::endl;
     int best_action = argmax(q_table, next_state, actions);
+    std::cout << "[QLearner::update] Done using argmax." << std::endl;
     q_table[current_state][current_action] += alpha * (reward_value + gamma * Q(next_state, best_action) - Q(current_state, current_action));
 }

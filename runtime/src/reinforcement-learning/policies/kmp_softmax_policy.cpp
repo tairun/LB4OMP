c5807b006ee1feccbf3a75622b9be3ca73f7ae8b
Luc Kury <luc.kury@unibas.ch>
2022-06-19 19:01:34 +0200

    Changed pointers in softmax

--- runtime/src/reinforcement-learning/policies/kmp_softmax_policy.cpp
+++ runtime/src/reinforcement-learning/policies/kmp_softmax_policy.cpp
@@ -14,7 +14,7 @@
 int SoftmaxPolicy::policy(int episode, int timestep, Agent* agent)
 {
     std::cout << "[SoftmaxPolicy::policy] Assembling weights ..." << std::endl;
-    std::vector<double> weights(*agent->get_table()[agent->get_current_state()], *agent->get_table()[agent->get_current_state()] + agent->get_action_space());
+    std::vector<double> weights((*agent->get_table())[agent->get_current_state()], (*agent->get_table())[agent->get_current_state()] + agent->get_action_space());
     std::cout << "[SoftmaxPolicy::policy] Calculating probabilities ..." << std::endl;
     auto probabilities = Softmax(weights, agent->get_tau());
     std::cout << "[SoftmaxPolicy::policy] Selecting an action ..." << std::endl;
